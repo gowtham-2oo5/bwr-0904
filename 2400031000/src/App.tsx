9f4ebfab6810f730c7597fa1a93bd3e0870d9f66
sahithi sritha <sahithisritha01@gmail.com>
2025-04-09 11:46:34 +0000

    31000-Submitting

---
+++ 2400031000/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className="text-3xl text-blue-300
+      font-bold">Hi I am Sahithi</h1>
+      <h2 className="text-5xl text-green-200">lalalalalalala</h2>
+      <h2 className="text-5xl text-yellow-100">okokokokok</h2>
+    </div>
+    
+  )
+}
+
+export default App
